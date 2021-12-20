d4e48f336b50047792d5a734ef4d1eac7b2764ff
Anthony S <tonesto7@gmail.com>
2021-12-20 14:57:56 -0500

    sync

--- docs/js/default.js
+++ docs/js/default.js
@@ -15,7 +15,7 @@ document.addEventListener(
             .then((source) => (installCode = source));
         document.querySelector('#copyInstaller').addEventListener('click', function (event) {
             Clipboard.copy(installCode);
-            alert('The installation code has been copied to your pasteboard.');
+            alert('The widget code has been copied to your devices clipboard.');
             event.preventDefault();
         });
 
@@ -23,15 +23,15 @@ document.addEventListener(
         if (document.location.search) {
             [].slice.call(document.querySelectorAll('[x-hide-install]')).map((el) => (el.style.display = 'none'));
             query = parseQuery(document.location.search);
-            document.querySelector('#scriptName').innerText = query.name;
-            document.querySelector('#scriptdudeInstaller').href += '&' + document.location.search.substr(1);
+            // document.querySelector('#scriptName').innerText = query.name;
+            // document.querySelector('#scriptdudeInstaller').href += '&' + document.location.search.substr(1);
             document.querySelector('#link').innerText = document.location.href;
             // document.querySelector('#markdown').innerText = '[![Download with ScriptDude](https://scriptdu.de/download.svg)](' + document.location.href + ')';
             // let html = '<a href="' + document.location.href + '"><img alt="Download with ScriptDude" src="https://scriptdu.de/download.svg"></a>';
             // let safeHtml = '<a href="#" target="_blank"><img alt="Download with ScriptDude" src="https://scriptdu.de/download.svg"></a>';
-            document.querySelector('#html').innerText = html;
-            document.querySelector('#markdown-preview').innerHTML = safeHtml;
-            document.querySelector('#html-preview').innerHTML = safeHtml;
+            // document.querySelector('#html').innerText = html;
+            // document.querySelector('#markdown-preview').innerHTML = safeHtml;
+            // document.querySelector('#html-preview').innerHTML = safeHtml;
         } else {
             [].slice.call(document.querySelectorAll('[x-hide-home]')).map((el) => (el.style.display = 'none'));
         }
